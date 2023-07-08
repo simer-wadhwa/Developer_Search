3f255e77f3a962eea976e4e65c8ac8fe49b5b256
simer-wadhwa <simerwadhwa115@gmail.com>
2023-07-08 12:46:00 -0700

    Database Queries

--- projects/views.py
+++ projects/views.py
@@ -1,35 +1,15 @@
 from django.shortcuts import render
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    proj=None
-    for i in projectsList:
-        if i['id'] == pk:
-            proj = i
-
-    context = {'project':proj}
+    project = Project.objects.get(id=pk)
+    context = {'project':project}
     return render(request,'projects/single_projects.html',context)
 
 
